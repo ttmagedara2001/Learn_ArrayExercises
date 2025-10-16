bfafde564ff1e993ca292629f7fc2589b5b01c38
ttmagedara2001 <thulanimagedara@gmail.com>
2025-10-16 22:33:18 +0530

    test

--- src/Test.java
+++ src/Test.java
@@ -10,16 +10,19 @@ public class Test {
         int[] arr = {2001,2002,2003,2004,2005,2006};
         int[] arr_new = new int[arr.length-1];
 
+        //Define the index of the element to be removed.
         int index = 2005;
 
                 System.out.println("Original Array : "+ Arrays.toString(arr));
 
+                //Loop to remove the element
                 for(int i = 0, k=0; i<arr.length ; i++){
                     if(arr[i]!=index){
                         arr_new[k] = arr[i];
                         k++;
                     }
                 }
+                //Print the modified array after removing the element.
                 System.out.println("After Removing the Element : "+ Arrays.toString(arr_new));
 
             }
